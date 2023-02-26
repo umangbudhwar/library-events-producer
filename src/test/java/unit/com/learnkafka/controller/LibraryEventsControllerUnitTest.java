a69bfaaf88fcfc204e292a18b2f9414e255e3d91
Umang Budhwar <umangbudhwar@gmail.com>
2023-02-27 00:08:43 +0530

    Added custom error messages using ControllerAdvice.

--- src/test/java/unit/com/learnkafka/controller/LibraryEventsControllerUnitTest.java
+++ src/test/java/unit/com/learnkafka/controller/LibraryEventsControllerUnitTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -50,6 +51,29 @@ public class LibraryEventsControllerUnitTest {
                         .content(json)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void postLibraryEvent_4xx() throws Exception {
+        Book book = Book.builder()
+                .bookId(null)
+                .bookAuthor(null)
+                .bookName("Kafka Using Spring Boot")
+                .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(book)
+                .build();
+
+        String json = objectMapper.writeValueAsString(libraryEvent);
+        doNothing().when(libraryEventProducer).sendLibraryEvent_Approach2(libraryEvent);
+        String expectedErrorMessage = "book.bookAuthor - must not be blank,book.bookId - must not be null";
+        mockMvc.perform(post("/v1/libraryevent")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().string(expectedErrorMessage));
     }
 
 }
