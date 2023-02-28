6700fa9b14ba144681f2f67dee1e082046d2ce23
Umang Budhwar <umangbudhwar@gmail.com>
2023-02-28 21:54:03 +0530

    Added unit test case for success scenario.

--- src/test/java/unit/com/learnkafka/producer/LibraryEventProducerUnitTest.java
+++ src/test/java/unit/com/learnkafka/producer/LibraryEventProducerUnitTest.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.learnkafka.domain.Book;
 import com.learnkafka.domain.LibraryEvent;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.TopicPartition;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -53,5 +55,35 @@ public class LibraryEventProducerUnitTest {
 
         when(kafkaTemplate.send(isA(ProducerRecord.class))).thenReturn(completableFuture);
         assertThrows(Exception.class, () -> eventProducer.sendLibraryEvent_Approach2(libraryEvent).get());
+    }
+
+    @Test
+    public void sendLibraryEvent_Approach2_success() throws JsonProcessingException, ExecutionException, InterruptedException {
+        Book book = Book.builder()
+                .bookId(null)
+                .bookAuthor(null)
+                .bookName("Kafka Using Spring Boot")
+                .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(book)
+                .build();
+
+        String record = objectMapper.writeValueAsString(libraryEvent);
+
+        ProducerRecord<Integer, String> producerRecord = new ProducerRecord<>("library-events", libraryEvent.getLibraryEventId(), record);
+        RecordMetadata recordMetadata = new RecordMetadata(new TopicPartition("library-events", 1),1, 1, 342, 1, 2);
+        SendResult<Integer, String> sendResult = new SendResult<>(producerRecord, recordMetadata);
+        CompletableFuture completableFuture = new CompletableFuture<>();
+        completableFuture.complete(sendResult);
+
+        when(kafkaTemplate.send(isA(ProducerRecord.class))).thenReturn(completableFuture);
+        CompletableFuture<SendResult<Integer, String>> completableFutureResult = eventProducer.sendLibraryEvent_Approach2(libraryEvent);
+
+        SendResult<Integer, String> sendResult1 = completableFutureResult.get();
+
+        assert sendResult1.getRecordMetadata().partition() == 1;
+
     }
 }
